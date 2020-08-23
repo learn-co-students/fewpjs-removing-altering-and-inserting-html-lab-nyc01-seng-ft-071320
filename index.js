5c91506847ec60011553ee53933e5b8c8ec987f6
David-Kirsch <davidjkirsch@gmail.com>
2020-08-23 18:11:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML = 'David is the champion'
