7326ad7938b507176ce9ab3ce6b515a0a2b743b6
iuriseara <iuriseara@gmail.com>
2020-08-23 13:23:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector('main#main');
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Iuri Seara is the champion'
+document.body.appendChild(newHeader);
