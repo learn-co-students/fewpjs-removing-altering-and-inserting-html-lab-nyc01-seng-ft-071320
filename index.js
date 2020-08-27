1a8786913ae61a747c82b77ac9f8c16eff3c39ac
stackguardian <mferdous98@aol.com>
2020-08-27 19:41:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 
+document.querySelector("main").remove()
 let newHeader = document.createElement("h1")
     newHeader.id = "victory"
     newHeader.innerHTML = "Jim is the champion"
